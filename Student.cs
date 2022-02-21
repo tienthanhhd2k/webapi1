419be3c7792e6bea95ef58f4091a2b317c47c8cc
TixnThxnh <97678796+tienthanhhd2k@users.noreply.github.com>
2022-02-21 20:19:25 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,13 @@
+namespace netcoreapi1
+{
+    public class Student
+    {
+        public string StudenName {get; set;}
+
+        public string University {get; set;}
+
+        public string Gender {get; set;}
+
+        public string BirthPlace {get; set;}
+    }
+}
